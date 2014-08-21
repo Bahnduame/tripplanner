30585388480759a9aa2ea35c368f9457836cea40
Bahnduame <jeffreylkim@gmail.com>
2014-08-21 11:41:13 -0400

    shit

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  models.Hotel.find(function(err, results) {
+  models.Hotel.find({}, function(err, results) {
     res.render('index', { hotels: results, title: "Trip Planner" });
   });
 });
